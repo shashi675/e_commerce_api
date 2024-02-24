485672f732f7ca3e1f664d7300022fbe6d9e1f44
shashi675 <shashikantrai675@gmail.com>
2024-02-24 18:00:48 +0530

    implement updateCart

--- routes/cart.js
+++ routes/cart.js
@@ -38,7 +38,7 @@ const addToCart = async (req, res) => {
 const showCart = async (req, res) => {
     try {
         const userName = req.userName;
-        
+
         // select cart items belonging to current user
         const q1 = "SELECT p.product_id, title, description, price, cat.category_id, category_name, c.quantity FROM cart_items AS c INNER JOIN products AS p ON c.product_id = p.product_id INNER JOIN categories AS cat ON p.category_id = cat.category_id WHERE user_name = $1";
 
@@ -50,11 +50,35 @@ const showCart = async (req, res) => {
 }
 
 
-const updateCart = (req, res) => {
+const updateCart = async (req, res) => {
     try {
+        const userName = req.userName;
+        const { productId, quantity } = req.body;
+
+        if(!productId || !quantity) return res.status(206).json({message: "required productId and quantity"});
+
+        // check for the product availability
+        const q1 = "SELECT quantity FROM products WHERE product_id = $1";
+        const availableItem = await db.query(q1, [productId]);
+        // check if product exist
+        if(availableItem.rowCount === 0) return res.status(403).json({error: "no product found"});
+        // check for quantity
+        if(availableItem.rows[0].quantity < quantity) return res.status(405).json({error: "product available in less quantity", availableQuantity: availableItem.rows[0].quantity});
+
+        // check for the product in cart_items table
+        const q2 = "SELECT cart_id FROM cart_items WHERE user_name = $1 AND product_id = $2";
+        const cartItem = await db.query(q2, [userName, productId]);
+        // product not available
+        if(cartItem.rowCount === 0) return res.status(403).json({error: "Item not found in cart. Please add first."});
+
+        // update the quantity of product
+        const q3 = "UPDATE cart_items SET quantity = $1 WHERE cart_id = $2";
+        const cartId = cartItem.rows[0].cart_id;
+        const insertItem = await db.query(q3, [quantity, cartId]);
+        res.status(200).json({message: "item quantity updated successfully"});
         
     } catch (err) {
-        return res.status(500).json({error: "internal server error"});
+        return res.status(500).json({error: err.message});
     }
 }
 
@@ -72,7 +96,7 @@ const removeCartItemByProductId = async (req, res) => {
 
 router.post("/addToCart", jwtAuthMiddleWare, addToCart);
 router.get("/viewCart", jwtAuthMiddleWare, showCart);
-router.post("/updateCart", jwtAuthMiddleWare, updateCart);
+router.put("/updateCart", jwtAuthMiddleWare, updateCart);
 router.delete("/removeCartPoduct/:productId", jwtAuthMiddleWare,removeCartItemByProductId);
 
 
