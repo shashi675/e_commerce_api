6cf3ee8b130183a62f75ef1b005e0c6aa381af45
shashi675 <shashikantrai675@gmail.com>
2024-02-26 03:05:03 +0530

    prepare doc through swagger

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const productsRoute = require("./routes/products");
 const authRoute = require("./routes/auth");
 const cartRoute = require("./routes/cart");
 const ordersRoute = require("./routes/orders");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 
 app.use(express.json());
@@ -20,6 +22,26 @@ app.use("/api/products", productsRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/cart", cartRoute);
 app.use("/api/orders", ordersRoute);
+
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'e-commerce-api using Node, Express and PostgreSQL',
+            version: '1.0.0'
+        },
+        servers: [
+            {
+                url: 'http://localhost:3001/'
+            }
+        ]
+    },
+    apis: ['./docs/swagger.yaml', './routes/*.js']
+}
+
+const swaggerSpec = swaggerJsDoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const PORT = 3001;
 app.listen(PORT, () => {
